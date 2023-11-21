5b4875cdfdc099a6f5bf7770b641b13881b132af
Dennis Gadomski <dennis.gadomski@gmail.com>
2023-11-21 17:32:42 +0100

    add FlightLog model

---
+++ app/Models/FlightLog.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class FlightLog extends Model
+{
+    protected $table = 'FlightLog';
+
+    protected $keyType = 'string';
+
+    public $incrementing = false;
+
+    public $timestamps = false;
+
+
+    protected $fillable = [
+        'id',
+        'pilotId',
+        'createdAt',
+        'updatedAt'
+    ];
+}

af95596acca415cc3aa1ff9971ba2c5df8264559
Dennis Gadomski <dennis.gadomski@gmail.com>
2023-11-23 15:40:51 +0100

    add all fields in resource response

--- app/Http/Resources/FlightLogResource.php
+++ app/Http/Resources/FlightLogResource.php
@@ -15,7 +15,34 @@ class FlightLogResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            'id' => $this->id
+            'id' => $this->id,
+            'pilotId' => $this->pilotId,
+            'createdAt' => $this->createdAt,
+            'updatedAt' => $this->updatedAt,
+            'date' => $this->date,
+            'departurePlace' => $this->departurePlace,
+            'departureTime' => $this->departureTime,
+            'arrivalPlace' => $this->arrivalPlace,
+            'arrivalTime' => $this->arrivalTime,
+            'aircraft' => $this->aircraft,
+            'aircraftRegistration' => $this->aircraftRegistration,
+            'SE' => $this->SE,
+            'ME' => $this->ME,
+            'multiPilotTime' => $this->multiPilotTime,
+            'flightTime' => $this->flightTime,
+            'flightTimePic' => $this->flightTimePic,
+            'flightTimeCP' => $this->flightTimeCP,
+            'flightTimeDual' => $this->flightTimeDual,
+            'flightTimeInstructor' => $this->flightTimeInstructor,
+            'picName' => $this->picName,
+            'dayLanding' => $this->dayLanding,
+            'nightLanding' => $this->nightLanding,
+            'octNight' => $this->octNight,
+            'octIFR' => $this->octIFR,
+            'fstdDate' => $this->fstdDate,
+            'fstdType' => $this->fstdType,
+            'fstdTime' => $this->fstdTime,
+            'remarks' => $this->remarks,
         ];
     }
 }
